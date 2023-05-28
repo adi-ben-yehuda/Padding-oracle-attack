7c2633754471fb38875039d1136747db9200a1c1
adi-ben-yehuda <adiby2017@gmail.com>
2023-05-28 15:28:09 +0300

    finish

--- ex1.py
+++ ex1.py
@@ -26,25 +26,21 @@ if __name__ == '__main__':
     if len(arguments) < 4:
         exit(1)
 
-    # ciphertext = binascii.unhexlify("83e10d51e6d122ca3faf089c7a924a7b")
-    # KEY = ("mydeskey").encode('ascii')
-    # IV = binascii.unhexlify("0000000000000000")
-
-    # ciphertext = binascii.unhexlify(arguments[1])
-    # KEY = (arguments[2]).encode('ascii')
-    # IV = binascii.unhexlify(arguments[3])
+    # ciphertext = bytes.fromhex("4e301349b6704658fcb5fb7dabf34e206e3e1223b86c1b4e360d69dcac04ac4e")
+    # KEY = bytes("Aalenian".encode())
+    # IV = bytes.fromhex("8487ffc596953c48")
 
     ciphertext = bytes.fromhex(arguments[1])
-    KEY = (arguments[2]).encode('ascii')
+    KEY = bytes(arguments[2].encode())
     IV = bytes.fromhex(arguments[3])
 
     final_plain_text = b''
     numOfBlocks = int(len(ciphertext) / 8)
     zeros = bytearray(8)
 
-    for k in reversed(range(numOfBlocks)):
+    for k in range(numOfBlocks):
         c = zeros + ciphertext[k * 8:(k+1)*8]
-        
+        block = b''
         for i in reversed(range(8)):
             while (oracle(c, KEY, IV) == False):
                 mutable_bytes = bytearray(c)
@@ -54,17 +50,18 @@ if __name__ == '__main__':
             if (k==0):
                 x = xor(8-i, bytearray(c)[i], bytearray(IV)[i])
             else:
-                x = xor(8-i, bytearray(c)[i], bytearray(ciphertext)[i])
-            final_plain_text = x + final_plain_text
+                x = xor(8-i, bytearray(c)[i], bytearray(ciphertext)[i+(k-1)*8])
+            block = x+ block
 
             mutable_bytes1 = bytearray(c)
             for j in range(i, 8):
                 if (k==0):
-                    x1 = xor(final_plain_text[j-i], 9-i, bytearray(IV)[j])
+                    x1 = xor(block[j-i], 9-i, bytearray(IV)[j])
                 else:
-                    x1 = xor(final_plain_text[j-i], 9-i, bytearray(ciphertext)[j])
+                    x1 = xor(block[j-i], 9-i, bytearray(ciphertext)[j+(k-1)*8])
                 mutable_bytes1[j] = bytes.fromhex(x1.hex())[0]
             c = bytes(mutable_bytes1)
+        final_plain_text = final_plain_text + block
 
     decrypt_plain_text = bytes(unpad(final_plain_text, DES.block_size))
     print(decrypt_plain_text.decode())
